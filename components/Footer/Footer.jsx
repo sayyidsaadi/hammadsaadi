c6217d725f38782ba6101cdaf725255acc9f0042
Sayyid Saadi <saadisyed271@gmail.com>
2023-08-26 14:49:09 +0600

    first commit

---
+++ components/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div className="mt-10 bg-color-bg-primary-variant text-center text-text-light-variant">
+      <p className="p-4 font-sans - serif font-500 text-md text-text-light-variant 3xl:text-2xl 3xl:p-8">
+        All rights reserved &copy; 2023 Hammad Sadi
+      </p>
+    </div>
+  );
+}
+
+export default Footer;
